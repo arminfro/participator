4aed2ab21ecd9ed4ac43e894eb3717e52d78eded
Armin Fröhlich <mail@arminfroehlich.de>
2021-02-02 14:13:39 +0100

    add SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { NextModule } from './server/nextjs/next.module';
-// import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './server/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const options = new DocumentBuilder()
-  //   .setTitle('Participator')
-  //   .setDescription('The participator API description')
-  //   .setVersion('1.0')
-  //   .addTag('participator')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, options);
-  // SwaggerModule.setup('api', app, document);
+  const options = new DocumentBuilder()
+    .setTitle('Participator')
+    .setDescription('The participator API description')
+    .setVersion('1.0')
+    .addTag('participator')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   app
     .get(NextModule)
