653366dc5dd00ea6c70dac2fd4be17899d0168c6
Robert Alstetter <robert.alstetter@gmail.com>
2021-02-23 15:46:20 +0100

    solve issue #3

--- src/pages/rooms/chat-input-form.tsx
+++ src/pages/rooms/chat-input-form.tsx
@@ -1,9 +1,4 @@
-import React, {
-  useState,
-  Dispatch,
-  SetStateAction,
-  ReactElement,
-} from 'react';
+import React, { useState, Dispatch, SetStateAction, ReactElement } from 'react';
 import User from '../../types/user';
 import Dropdown from './dropdown';
 
@@ -13,10 +8,17 @@ interface Props {
   preSetInput: string;
   setInput: Dispatch<SetStateAction<string>>;
   users?: User[];
-  allowEscape: boolean
+  allowEscape: boolean;
 }
 
-export default function ChatInputForm({ onCreate, onCancel, setInput, preSetInput, users, allowEscape }: Props): ReactElement {
+export default function ChatInputForm({
+  onCreate,
+  onCancel,
+  setInput,
+  preSetInput,
+  users,
+  allowEscape,
+}: Props): ReactElement {
   const [userInput, setUserInput] = useState(preSetInput);
   const [oldMsg, setOldMsg] = useState(userInput);
 
@@ -24,17 +26,20 @@ export default function ChatInputForm({ onCreate, onCancel, setInput, preSetInpu
   const [doAtMention, setDoAtmention] = useState(false);
   const [action, setAction] = useState('');
   const [typeAhead, setTypeAhead] = useState('');
-  const [reducedUserList, setReducedUserList] = useState(users)
+  const [reducedUserList, setReducedUserList] = useState(users);
+  const [caretPosition, setCaretPosition] = useState(0);
 
   // const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  const onClickSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+  const onClickSubmit = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ): void => {
     e.preventDefault();
     if (preSetInput !== '') {
-      userInput.replace("\n", " \n");
+      userInput.replace('\n', ' \n');
     }
     onCreate(userInput, setInput);
     setInput('');
-    setUserInput('')
+    setUserInput('');
   };
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
@@ -46,10 +51,9 @@ export default function ChatInputForm({ onCreate, onCancel, setInput, preSetInpu
     onCancel();
   };
 
-
   const isLetter = (char: string): boolean => {
-    return (/[a-zA-Z]/).test(char) && char.length == 1
-  }
+    return /[a-zA-Z]/.test(char) && char.length == 1;
+  };
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>): void => {
     if (doAtMention) {
@@ -57,44 +61,60 @@ export default function ChatInputForm({ onCreate, onCancel, setInput, preSetInpu
       if (e.key == 'Backspace') {
         if (userInput.length > 0) {
           if (userInput[userInput.length - 1] === '@') {
-            resetAtMention()
+            resetAtMention();
           }
-          const input_ = userInput.substr(0, userInput.length - 1)
-          setUserInput(input_)
+          const input_ = userInput.substr(0, userInput.length - 1);
+          setUserInput(input_);
           if (typeAhead.length > 0) {
-            const tempTypeAhead = typeAhead.substr(0, typeAhead.length - 1)
-            setTypeAhead(currentTypeAhead => currentTypeAhead.substr(0, currentTypeAhead.length - 1))
-            setReducedUserList(users.filter(e => e.name.toLowerCase().startsWith(tempTypeAhead)));
+            const tempTypeAhead = typeAhead.substr(0, typeAhead.length - 1);
+            setTypeAhead((currentTypeAhead) =>
+              currentTypeAhead.substr(0, currentTypeAhead.length - 1),
+            );
+            setReducedUserList(
+              users.filter((e) =>
+                e.name.toLowerCase().startsWith(tempTypeAhead),
+              ),
+            );
           }
         }
       } else if (e.key == 'Escape') {
-        resetAtMention()
+        resetAtMention();
         onCancel(oldMsg);
       } else if (isLetter(e.key)) {
-        const tempTypeAhead = typeAhead + e.key
-        setTypeAhead(currentTypeAhead => currentTypeAhead + e.key)
-        setReducedUserList(users.filter(e => e.name.toLowerCase().startsWith(tempTypeAhead)));
-        setUserInput(currentInput => currentInput + e.key)
+        const tempTypeAhead = typeAhead + e.key;
+        setTypeAhead((currentTypeAhead) => currentTypeAhead + e.key);
+        setReducedUserList(
+          users.filter((e) => e.name.toLowerCase().startsWith(tempTypeAhead)),
+        );
+        setUserInput((currentInput) => currentInput + e.key);
       }
       setAction(e.key);
     }
 
     if (e.key == 'Enter' && e.ctrlKey) {
-      userInput.replace("\n", " \n");
+      userInput.replace('\n', ' \n');
       onCreate(userInput, () => setUserInput(''));
     } else if (allowEscape && e.key == 'Escape') {
       onCancel(oldMsg);
     } else if (e.key == '@') {
-      setOldMsg(userInput)
-      setDoAtmention(true)
+      if (!/\w/.test(userInput.charAt(e.target.selectionStart - 1))) {
+        setOldMsg(userInput);
+        setDoAtmention(true);
+        setCaretPosition(e.target.selectionStart);
+      }
     }
   };
 
   const selectAtMention = (index: number, value: string): void => {
-    const addSpace = oldMsg.charAt(oldMsg.length - 1) !== ' ' ? ' ' : '';
-    setUserInput(oldMsg +
-      addSpace +
-      '**@' + value + '** ');
+    const addSpace = oldMsg.charAt(caretPosition - 1) !== ' ' ? ' ' : '';
+    setUserInput(
+      oldMsg.slice(0, caretPosition) +
+        addSpace +
+        '**@' +
+        value +
+        '** ' +
+        oldMsg.slice(caretPosition),
+    );
     resetAtMention();
   };
 
@@ -103,10 +123,10 @@ export default function ChatInputForm({ onCreate, onCancel, setInput, preSetInpu
     setDoAtmention(false);
     setReducedUserList(users);
     setTypeAhead('');
-  }
+  };
 
   return (
-    <form className="ui reply form" >
+    <form className="ui reply form">
       <div className="dropdown-container">
         <textarea
           value={userInput}
@@ -115,20 +135,33 @@ export default function ChatInputForm({ onCreate, onCancel, setInput, preSetInpu
           placeholder="Your Message"
           id="msg"
         />
-        {doAtMention && <Dropdown entries={reducedUserList.map((u) => u.name)} callback={selectAtMention} action={action} />}
+        {doAtMention && (
+          <Dropdown
+            entries={reducedUserList.map((u) => u.name)}
+            callback={selectAtMention}
+            action={action}
+          />
+        )}
       </div>
-      <button onClick={onClickSubmit} className="ui blue labled submit icon button ">
+      <button
+        onClick={onClickSubmit}
+        className="ui blue labled submit icon button "
+      >
         <i className="icon edit"></i>send
       </button>
       <span className="actions">
         or <b>Ctrl-Return</b>
       </span>
 
-      {allowEscape &&
-        <button type="button" onClick={onClickCancel} className="ui red labled submit icon button ">
+      {allowEscape && (
+        <button
+          type="button"
+          onClick={onClickCancel}
+          className="ui red labled submit icon button "
+        >
           <i className="icon cancel"></i>cancel
         </button>
-      }
+      )}
     </form>
   );
 }
