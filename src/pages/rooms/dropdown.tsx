1cfac24b1357d5ca0927a6e94b3f3c6b17c595fa
Robert Alstetter <robert.alstetter@gmail.com>
2021-02-24 13:59:53 +0100

    dropdown no longer displayed if no entries exist

--- src/pages/rooms/dropdown.tsx
+++ src/pages/rooms/dropdown.tsx
@@ -1,51 +1,59 @@
 import React, { ReactElement, useEffect, useRef, useState } from 'react';
 
 export interface Position {
-  x: number,
-  y: number
+  x: number;
+  y: number;
 }
 
 interface Props {
   entries: string[];
-  callback: (index: number, value: string) => void
-  action: string
+  callback: (index: number, value: string) => void;
+  action: string;
 }
 
-
-export default function Dropdown({ entries, callback, action }: Props): ReactElement {
+export default function Dropdown({
+  entries,
+  callback,
+  action,
+}: Props): ReactElement {
   const [selected, setSelected] = useState(0);
   const dropdownRef = useRef(null);
 
   useEffect(() => {
-    onAction(action)
-  }, [action])
+    onAction(action);
+  }, [action]);
 
-  const select = (
-    i: number,
-    element: string
-  ) => { callback(i, element); };
+  const select = (i: number, element: string) => {
+    callback(i, element);
+  };
 
   const onAction = (action: string): void => {
     if (action == 'Enter') {
       select(selected, entries[selected]);
     } else if (action == 'ArrowDown') {
-      setSelected((selected + 1) % entries.length)
+      setSelected((selected + 1) % entries.length);
     } else if (action == 'ArrowUp') {
-      setSelected((Math.abs(selected - 1)) % entries.length)
+      setSelected(Math.abs(selected - 1) % entries.length);
     }
   };
 
   return (
-    <div className="dropdown" >
-      <div className="dropdowntext" ref={dropdownRef}>
-        {entries.map((e, i) => {
-          return (
-            <div key={i} className={(selected == i) && "selected"} onClick={(ev) => select(i, e)}>
-              {e}
-            </div>
-          );
-        })}
+    entries.length > 0 && (
+      <div className="dropdown">
+        <div className="dropdowntext" ref={dropdownRef}>
+          {entries.map((e, i) => {
+            return (
+              <div
+                key={i}
+                className={selected == i ? 'selected' : undefined}
+                onClick={(ev) => select(i, e)}
+              >
+                {e}
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div >
+    )
   );
 }
